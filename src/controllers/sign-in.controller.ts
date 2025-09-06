abcfb6868c43c368bd7324826532c96ec7dc97c1
Renan Pereira Loureiro <renan.pereiral@sonda.com>
2025-09-06 13:40:20 -0300

    refactor: remover controladores de sign-in e sign-up obsoletos

--- src/controllers/sign-in.controller.ts
+++
@@ -1,8 +0,0 @@
-export class SignInController {
-  static async handle() {
-    return {
-      statusCode: 200,
-      body: JSON.stringify({ accessToken: "1234567890" }),
-    };
-  }
-}
--- src/controllers/sign-up.controller.ts
+++
@@ -1,8 +0,0 @@
-export class SignUpController {
-  static async handle() {
-    return {
-      statusCode: 201,
-      body: JSON.stringify({ accessToken: "1234567890" }),
-    };
-  }
-}
