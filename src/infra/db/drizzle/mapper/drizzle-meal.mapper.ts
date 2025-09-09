4d6e511e8210ae29fb9139a9f6500938d0956a50
Renan Pereira Loureiro <renan.pereiral@sonda.com>
2025-09-09 03:04:34 -0300

    fix: garantir que propriedades opcionais de refeição sejam tratadas corretamente ao persistir dados, evitando erros de acesso a propriedades indefinidas

--- src/infra/db/drizzle/mapper/drizzle-meal.mapper.ts
+++ src/infra/db/drizzle/mapper/drizzle-meal.mapper.ts
@@ -28,13 +28,13 @@ export class DrizzleMealMapper {
   static toPersistence(meal: Meal): DrizzleMeal {
     return {
       id: meal.id.toValue(),
-      name: meal.name,
-      icon: meal.icon,
+      name: meal?.name ?? '',
+      icon: meal?.icon ?? '',
       userId: meal.userId.toValue(),
       status: meal.status,
       inputType: meal.inputType,
       inputFileKey: meal.inputFileKey,
-      foods: meal.foods as Array<unknown>,
+      foods: (meal?.foods as Array<unknown>) ?? [],
       createdAt: toUTC(meal.createdAt),
       updatedAt: toUTC(meal.updatedAt),
     };
